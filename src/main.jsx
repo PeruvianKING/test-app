caf603accfc4936c2e3b1c92db875ad7ed65db9d
PeruvianKING <peruvianking@proton.me>
2026-01-17 14:17:10 +0100

    feat: Implement Google Analytics for page view tracking.

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import Analytics from './components/Analytics'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <Analytics />
       <App />
     </BrowserRouter>
   </StrictMode>,
